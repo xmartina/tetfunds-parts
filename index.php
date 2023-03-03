74105be4238fcae23d159e3102bcb67f671bc806
unknown <martinsprince126@gmail.com>
2023-03-03 19:08:51 +0100

    init push

---
+++ index.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>TetFund Project Page</title>
+</head>
+<body>
+    <div class="projects-section">
+        <div class="container">
+            <div class="row">
+<!--                TetFunds Navigation -->
+                <div class="col-lg-3">
+                    <div class="nav-menu-wrapper">
+                        <div class="row">
+                            <div class="col-lg-8">
+                                <h2 class="nav-title"> Special Interventions </h2>
+                            </div>
+                            <div class="col-lg-3">
+                                <span class="nav-title-icon divide-icon-gray"></span>
+                            </div>
+                        </div>
+                        <div class="nav-menu-list">
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-7 d-flex align-items-center justify-start justify-content-start">Zonal Intervention</div>
+                                    <div class="col-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Disaster Recovery</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">High Impact Intervention</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">National Research Fund</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <h2 class="nav-title">Annual Intervention</h2>
+                                </div>
+                                <div class="col-lg-3">
+                                    <span class="nav-title-icon icon-grid"></span>
+                                </div>
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Academic Manuscript Development</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Entrepreneurship</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Equipment Fabrication</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Tetfund Project Maintanance</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Academic Research Journal</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">ICT Support</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Institution Based Research</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Teaching Practice</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Conference Attendance</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Library Development</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Academic Staff Training</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                            <div class="list-menu">
+                                <div class="row d-flex align-items-center justify-content-start">
+                                    <div class="col-lg-2"> <span class="Physical-infrastructure-icon"></span></div>
+                                    <div class="col-lg-7 d-flex align-items-center justify-start justify-content-start">Physical Infrastructure</div>
+                                    <div class="col-lg-2"><span class="nav-icon-right-styling right-arrow-icon-darkgray"></span></div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<!--                TetFunds Data Display-->
+                <div class="col-lg-8">
+                    <div class="data-display-wrapper">
+                        <div class="row gy-5 gx-5">
+                            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 ">
+                                <div class="tetfund-data-box">
+                                    <div class="data-img-box senate-building-img"></div>
+                                    <div class="content-box">
+                                        <div class="title">SENATE BUILDING</div>
+                                        <div class="content">
+                                            This is a year 2014 intervention project that started in 2015 but
+                                        </div>
+                                    </div>
+                                    <div class="button-box">
+                                        <button class="btn-custom">View Project <div class="right-arrow-icon-dark btn-arrow-theme"></div></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 mt-sm-30">
+                                <div class="tetfund-data-box">
+                                    <div class="data-img-box senate-building-img"></div>
+                                    <div class="content-box">
+                                        <div class="title">SENATE BUILDING</div>
+                                        <div class="content">
+                                            This is a year 2014 intervention project that started in 2015 but
+                                        </div>
+                                    </div>
+                                    <div class="button-box">
+                                        <button class="btn-custom">View Project <div class="right-arrow-icon-dark btn-arrow-theme"></div></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 mt-sm-30">
+                                <div class="tetfund-data-box">
+                                    <div class="data-img-box senate-building-img"></div>
+                                    <div class="content-box">
+                                        <div class="title">SENATE BUILDING</div>
+                                        <div class="content">
+                                            This is a year 2014 intervention project that started in 2015 but
+                                        </div>
+                                    </div>
+                                    <div class="button-box">
+                                        <button class="btn-custom">View Project <div class="right-arrow-icon-dark btn-arrow-theme"></div></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 mt-30">
+                                <div class="tetfund-data-box">
+                                    <div class="data-img-box senate-building-img"></div>
+                                    <div class="content-box">
+                                        <div class="title">SENATE BUILDING</div>
+                                        <div class="content">
+                                            This is a year 2014 intervention project that started in 2015 but
+                                        </div>
+                                    </div>
+                                    <div class="button-box">
+                                        <button class="btn-custom">View Project <div class="right-arrow-icon-dark btn-arrow-theme"></div></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 mt-30">
+                                <div class="tetfund-data-box">
+                                    <div class="data-img-box senate-building-img"></div>
+                                    <div class="content-box">
+                                        <div class="title">SENATE BUILDING</div>
+                                        <div class="content">
+                                            This is a year 2014 intervention project that started in 2015 but
+                                        </div>
+                                    </div>
+                                    <div class="button-box">
+                                        <button class="btn-custom">View Project <div class="right-arrow-icon-dark btn-arrow-theme"></div></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
